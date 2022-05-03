61a051a8b98ff8bd195a29e4d5f0c063058a2f33
CristiPV <cristi.valentin29@gmail.com>
2022-05-03 12:59:20 +0200

    add input field theme

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -1,11 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
 import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 export const theme = extendTheme({
   colors: {
     primary: "#19169C",
     secondaryText: "#ffffff",
     attendColor: "green",
+    inputBackground: "#F0F0F1",
+    inputBorder: "#606060",
+    inputPlaceholder: "#616060"
   },
-  components: { Button },
+  components: { Button, Input },
 });

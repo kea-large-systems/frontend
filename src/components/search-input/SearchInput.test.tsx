41b77a8e58308782f83c26b082bfeae17207bf8d
CristiPV <cristi.valentin29@gmail.com>
2022-05-03 13:50:27 +0200

    add describe on search input test

--- src/components/search-input/SearchInput.test.tsx
+++ src/components/search-input/SearchInput.test.tsx
@@ -1,24 +1,22 @@
-import {render} from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { SearchInput } from "./SearchInput";
 import userEvent from "@testing-library/user-event";
 
-test("checks if the component has a search field", () => {
-    const {getByPlaceholderText} = render(
-        <SearchInput />
-    );
+describe("checks that the search input field is being loaded correctly", () => {
+  test("checks if the component has a search field", () => {
+    const { getByPlaceholderText } = render(<SearchInput />);
 
     expect(getByPlaceholderText("Search")).toBeVisible();
-});
+  });
 
-test("checks if the search field can be typed in", () => {
+  test("checks if the search field can be typed in", () => {
     const expectedValue = "Hello World!";
 
-    const {getByPlaceholderText} = render(
-        <SearchInput />
-    );
+    const { getByPlaceholderText } = render(<SearchInput />);
 
     const inputField = getByPlaceholderText("Search") as HTMLInputElement;
     userEvent.type(inputField, expectedValue);
 
     expect(inputField).toHaveValue(expectedValue);
+  });
 });

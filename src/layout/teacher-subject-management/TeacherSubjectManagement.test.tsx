c9784b895e691fbd05f17cb16fe58f1997ad53cf
CristiPV <cristi.valentin29@gmail.com>
2022-05-03 12:58:59 +0200

    add teacher subject management page

---
+++ src/layout/teacher-subject-management/TeacherSubjectManagement.test.tsx
@@ -0,0 +1,16 @@
+
+
+export default describe("checks if the teacher subject management page loads correctly", () => {
+    
+    // test("checks if the page has an add button", () => {
+      
+    // });
+
+    // test("checks if the add button redirects to the correct page", () => {
+      
+    // });
+
+    // test("checks if the page has a subject table with the appropriate columns", () => {
+
+    // });
+});
---
+++ src/layout/teacher-subject-management/TeacherSubjectManagement.tsx
@@ -0,0 +1,10 @@
+import {HStack} from "@chakra-ui/react";
+import {SearchInput} from "../../components/search-input/SearchInput";
+
+export function TeacherSubjectManagement() {
+  return (
+    <HStack>
+        <SearchInput/>
+    </HStack>
+  );
+}

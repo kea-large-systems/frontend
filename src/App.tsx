004a78adda2b6a8a8c943c01ab2cdb4934c36cf3
CristiPV <cristi.valentin29@gmail.com>
2022-05-03 12:57:31 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { StudentHomepage } from "./layout/student-homepage/StudentHomepage";
 import { StudentNavigationBar } from "./layout/navigation-bar/student-navigation-bar/StudentNavigationBar";
+import { TeacherNavigationBar } from "./layout/navigation-bar/teacher-navigation-bar/TeacherNavigationBar";
+import { TeacherSubjectManagement } from "./layout/teacher-subject-management/TeacherSubjectManagement";
+import { addScaleCorrector } from "framer-motion";
 
 function App() {
   return (
     <BrowserRouter>
-      <StudentNavigationBar />
-      <StudentHomepage />
+      <Routes>
+        <Route
+          path="/"
+          element={[<StudentNavigationBar />, <StudentHomepage />]}
+        />
+        <Route
+          path="/manage-classes"
+          element={[<TeacherNavigationBar />, <TeacherSubjectManagement />]}
+        ></Route>
+      </Routes>
     </BrowserRouter>
   );
 }

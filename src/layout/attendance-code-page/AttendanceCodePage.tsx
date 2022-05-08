ee553a63d1081814bab8c85d2d07eca3612b0aa7
Teodor_Jonasson <teodor_jonasson@hotmail.com>
2022-05-08 14:20:08 +0200

    feat: 🎸 Init attendance code page

---
+++ src/layout/attendance-code-page/AttendanceCodePage.tsx
@@ -0,0 +1,12 @@
+import { Button, VStack } from "@chakra-ui/react";
+
+export function AttendanceCodePage() {
+  const onSubmit = () => {};
+  return (
+    <VStack>
+      <Button variant="attend" bgColor="#5D5FEF">
+        Check Attendance
+      </Button>
+    </VStack>
+  );
+}

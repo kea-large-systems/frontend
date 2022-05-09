e694dc954f7c40f897439a4903b98caa0f059b6a
CristiPV <cristi.valentin29@gmail.com>
2022-05-09 09:56:57 +0200

    remove unused import

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme/Theme";
-import { worker } from "./mocks/browser";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 
